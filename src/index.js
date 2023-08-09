1ac0439d0ee63c5d5727f2bf4567b0db56390130
ivi0540 <ivi0540@gmail.com>
2023-08-09 14:41:12 +0300

    feat: tusk towel-sort solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(m) {
+    if (m === undefined) { return []; };
+    let order = true;
+    let result = [];
+    for (arr of m) {
+        if (order) {
+            result = result.concat(arr);
+        } else {
+            result = result.concat(arr.reverse());
+        };
+        order = !order;
+    };
+    return result;
+};
